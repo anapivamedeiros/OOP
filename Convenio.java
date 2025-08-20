8943dbc31c4a9be0a90f7477de88202eda6acf73
anapivamedeiros <ana.piva1a@gmail.com>
2025-08-20 07:16:54 -0300

    Convenio.java
    Convenio.java

---
+++ Convenio.java
@@ -0,0 +1,26 @@
+public class Convenio {
+        String nome;
+        String plano;
+        double tarifa;
+
+        public  String getNome() {
+            return nome;
+        }
+        public void setNome(String nome){
+            this.nome= nome;
+        }
+        public String getPlano(){
+            return plano;
+        }
+        public void setPlano(String plano){
+            this.plano =plano;
+        }
+        public double getTarifa() {
+            return tarifa;
+        }
+        public  void setTarifa(double tarifa){
+            this.tarifa= tarifa;
+        }
+    }
+
+
