e21755ce7bfd27d4c7a0144c6853930a34a025da
anapivamedeiros <ana.piva1a@gmail.com>
2025-08-27 15:39:39 -0300

    Conta bancaria
    Conta bancaria

---
+++ ContaBancaria.java
@@ -0,0 +1,28 @@
+public class ContaBancaria {
+    private double saldo;
+
+    public ContaBancaria(double saldoInicial) {
+        if(saldoInicial >= 0) {
+            this.saldo = saldoInicial;
+        } else {
+            this.saldo = 0;
+        }
+    }
+    public double getSaldo(){
+        return saldo;
+    }
+    public void depositar(double valor){
+        if(valor > 0) {
+            saldo += valor;
+        } else {
+            System.out.println("Valor de deposito invalido!");
+        }
+    }
+    public void sacar (double valor){
+        if(valor > 0 && valor <= saldo){
+            saldo -= valor;
+        } else {
+            System.out.println("Saque inválido! Verifique o valor.");
+        }
+    }
+}
