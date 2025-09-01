83c12c9792d138f7b4fb93dc9c8f1f7cee24645b
anapivamedeiros <ana.piva1a@gmail.com>
2025-09-01 14:21:41 -0300

    Funcionario
    Funcionario

---
+++ PessoaFunPrincipal.java
@@ -0,0 +1,12 @@
+package Heranca;
+
+public class PessoaFunPrincipal {
+        public static void main(String[] args) {
+            Funcionario funcionario = new Funcionario("João", "123.456.789-00", 2500.0);
+            funcionario.exibirInfo();
+
+            funcionario.aumentarSalario(10);
+            funcionario.exibirInfo();
+        }
+    }
+
